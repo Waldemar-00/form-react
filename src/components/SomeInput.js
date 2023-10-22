41328cdf868afb3e393c158bad5cee6ea3f3e7cc
uladzimir <01myit@gmail.com>
2023-10-22 15:52:42 +0300

    input with useSate and useRef

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,9 +1,23 @@
-const SomeInput = (props) => {
+import { useState, useRef } from 'react'
+const SomeInput = () => {
+  const [name, setName] = useState('')
+  const nameRef = useRef()
+  function changeName(e) {
+    setName(e.target.value)
+  }
+  function submitForm(e) {
+    e.preventDefault()
+    console.log(nameRef.current.value, name)
+  }
   return (
-    <form>
+    <form onSubmit={(e) => submitForm(e)}>
       <div className="form-control">
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input type="text"
+          id="name"
+          ref={nameRef}
+          value={name}
+          onInput={(e) => changeName(e)} />
       </div>
       <div className="form-actions">
         <button>Отправить</button>
