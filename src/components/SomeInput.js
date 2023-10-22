681dd9bab44885d6b777d2203e4b3fbdeeb7d208
uladzimir <01myit@gmail.com>
2023-10-22 17:23:32 +0300

    added more complex validation

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,30 +1,46 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 const SomeInput = () => {
   const [name, setName] = useState('')
-  const [isNameValid, setIsNameValid] = useState(true)
+  const inputRef = useRef()
+  const [isNameValid, setIsNameValid] = useState(false)
+  const [isTouch, setIsTouch] = useState(false)
+  function changeTouch() {
+    setIsTouch(true)
+  }
+  function blurValidate() {
+    if (name.length < 1) {
+      setIsNameValid(true)
+    }
+  }
   function changeName(e) {
-    setName(e.target.value)
+    setName(e.target.value.trim())
+    if (inputRef.current.value.length > 2) setIsNameValid(false)
+    else setIsNameValid(true)
   }
   function submitForm(e) {
     e.preventDefault()
+    setIsTouch(true)
     if (name.trim() === '') {
-      setIsNameValid(false)
+      setIsNameValid(true)
       return
     }
-    setIsNameValid(true)
+    setIsNameValid(false)
     setName('')
   }
-  const inputClass = isNameValid ? "form-control" : "form-control invalid"
+  const inputClass = !isNameValid || !isTouch ? "form-control" : "form-control invalid"
   return (
     <form onSubmit={(e) => submitForm(e)}>
       <div className={inputClass}>
         <label htmlFor="name">Enter your Name</label>
         <input type="text"
           id="name"
+          ref={inputRef}
           value={name}
+          onFocus={(e) => changeTouch(e)}
+          onBlur={blurValidate}
           onInput={(e) => changeName(e)} />
       </div>
-      {!isNameValid && <p className="error-text">Enter your Name!!!</p>}
+      { isNameValid && isTouch && <p className="error-text">Enter your Name, more than three sign!!!</p> }
       <div className="form-actions">
         <button>Отправить</button>
       </div>
