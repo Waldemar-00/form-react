ecf05ac5b2e76c8025afdee0f8ef7ca0c13dcd95
uladzimir <01myit@gmail.com>
2023-10-30 16:03:07 +0300

    added form validation, have to a refactoring

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,20 +1,20 @@
 import useInputValidation from '../hooks/useInputValidtion'
-export default function Input({ id, type, validate }) {
+export default function Input({ id, type, validate_1, validate_2, validate_3 }) {
   const { isValidate, inputStyle, checkFocus, checkBlur, checkValidation } = useInputValidation()
   let content
   function returnValidation() {
     switch (id) {
       case 'name':
         content = <p>Enter more than two characters!</p>
-        validate(isValidate) 
+        validate_1(isValidate) 
         break
       case 'surname':
         content = <p>Enter more than two characters!</p>
-        validate(isValidate)
+        validate_2(isValidate)
         break
       case 'email':
         content = <p>Enter more than two characters<br /> And Don't forget '@'!</p>
-        validate(isValidate)
+        validate_3(isValidate)
         break
       default: return
     }
@@ -27,7 +27,10 @@ export default function Input({ id, type, validate }) {
       <input
         id={id}
         type={type}
-        onFocus={(e) => checkFocus(e)}
+        onFocus={(e) => {
+          
+          checkFocus(e)
+        }}
         onBlur={(e) => {
           checkBlur(e)
           returnValidation()
--- src/components/SomeForm.js
+++ src/components/SomeForm.js
@@ -1,20 +1,29 @@
 import Input from './Input'
 import Button from './Button'
 import { useState } from 'react'
+
 function SomeForm() {
-  console.log('render')
-  const [validation, setValidation] = useState()
-  function validate(valid) {
-    setValidation(validation => validation = valid)
+  const [validation_1, setValidation_1] = useState(true)
+  const [validation_2, setValidation_2] = useState(true)
+  const [validation_3, setValidation_3] = useState(true)
+  function validate_1(isValidate) {
+    setValidation_1(validation_1 => validation_1 = !isValidate)
   }
+  function validate_2(isValidate) {
+    setValidation_2(validation_2 => validation_2 = !isValidate)
+  }
+  function validate_3(isValidate) {
+    setValidation_3(validation_3 => validation_3 = !isValidate)
+  }
+  console.log(validation_1, validation_2, validation_3)
   return (
     <form>
       <div className="control-group">
-        <Input type="text" id="name" validate={validate}/>
-        <Input type="text" id="surname" validate={validate}/>
-        <Input type="email" id="email" validate={validate}/>
+        <Input type="text" id="name" validate_1={validate_1}/>
+        <Input type="text" id="surname" validate_2={validate_2}/>
+        <Input type="email" id="email" validate_3={validate_3}/>
         <div className="form-actions">
-        <Button disabled={!validation}>Submit</Button>
+          <Button disabled={validation_1 || validation_2 || validation_3}>Submit</Button>
         </div>
       </div>
     </form>
