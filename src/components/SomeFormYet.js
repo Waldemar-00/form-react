165f77c1ccde16f454134e899b96bdf62de232d7
uladzimir <01myit@gmail.com>
2023-10-31 16:19:03 +0300

    added fetch

--- src/components/SomeFormYet.js
+++ src/components/SomeFormYet.js
@@ -23,9 +23,38 @@ function SomeFormYet() {
   } = useInputValidation()
   const contentName = <p>Enter more than two characters!</p> 
   const contentEmail = <p>Enter more than two characters<br /> And Don't forget '@'!</p>
-  const isDisabled = !isValidateName || !isValidateSurname || !isValidateEmail ? true : false
+  let isDisabled = true
+  if (isValidateName && isValidateSurname && isValidateEmail) {
+    isDisabled = false
+  }
+  const objectData = {}
+  function createDataAfterBlur(e) {
+    switch (e.target.id) {
+      case 'name': 
+        objectData.name = e.target.value
+        break
+      case 'surname': 
+        objectData.surname = e.target.value
+        break
+      case 'email': 
+        objectData.email = e.target.value
+        break
+      default: return 
+    }
+  }
+  function submitForm(e) {
+    e.preventDefault()
+    if (objectData.name && objectData.surname && objectData.email) {
+      fetch('https://custom-hooks-firebase-default-rtdb.firebaseio.com/products.json', {
+        method: 'POST',
+        body: JSON.stringify(objectData),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      document.querySelector('form').reset()
+    } else return
+  }
   return (
-    <form>
+    <form onSubmit={(e) => submitForm(e)}>
       <div className="control-group">
         <div className={inputStyleName}>
           <label htmlFor="name">Name</label>
@@ -33,7 +62,10 @@ function SomeFormYet() {
             type="text"
             id="name"
             onFocus={(e) => checkFocusName(e)}
-            onBlur={(e) => checkBlurName(e)}
+            onBlur={(e) => {
+              createDataAfterBlur(e)
+              checkBlurName(e)
+            }}
             onChange={(e) => checkValidationName(e, 0)}
           />
           {!isValidateName && contentName}
@@ -44,7 +76,10 @@ function SomeFormYet() {
             type="text"
             id="surname"
             onFocus={(e) => checkFocusSurname(e)}
-            onBlur={(e) => checkBlurSurname(e)}
+            onBlur={(e) => {
+              createDataAfterBlur(e)
+              checkBlurSurname(e)
+            }}
             onChange={(e) => checkValidationSurname(e, 0)}
           />
           {!isValidateSurname && contentName}
@@ -56,7 +91,10 @@ function SomeFormYet() {
           type="email"
           id="email"
           onFocus={(e) => checkFocusEmail(e)}
-          onBlur={(e) => checkBlurEmail(e)}
+          onBlur={(e) => {
+            createDataAfterBlur(e)
+            checkBlurEmail(e)
+          }}
           onChange={(e) => checkValidationEmail(e, 0)}
         />
         {!isValidateEmail && contentEmail}
